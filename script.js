1ae3d8f020ae47fbfa310aa81da866495c448d6a
yoeldesdev <yoeldesdev@gmail.com>
2024-06-15 15:14:27 +0700

    new web

---
+++ script.js
@@ -0,0 +1,25 @@
+// redir s
+function redirIg() {
+    window.location.href = "https://instagram.com/yoel.desamin?igshid=NTc4MTIwNjQ2YQ=="
+}
+function redirWa() {
+    window.location.href = "https://wa.me//+6285163725418"
+}
+function redirDc() {
+    window.location.href = "https://discord.com/users/898158406278479932"
+}
+function redirFb() {
+    window.location.href = "https://www.facebook.com/starli.creators?mibextid=ZbWKwL"
+}
+// redir e
+
+// loader s
+const loaderWrapperEl = document.querySelector('.loader-wrapper')
+
+setTimeout(() => {
+    loaderWrapperEl.style.opacity = '0'
+}, 1500);
+
+setTimeout(() => {
+    loaderWrapperEl.style.display = 'none'
+}, 1700)
